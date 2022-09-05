8140011907c703049cb6b1de31061f82a63d38ae
Roman Smolnyk <poma23324@gmail.com>
2022-09-05 23:37:48 +0300

    [refactor]

--- push.sh
+++ push.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 git push https://gitlab.com/roman-smolnyk/python-layout-converter.git master
+git push https://gitlab.com/roman-smolnyk/python-layout-converter.git --tags
 git push https://github.com/poma23324/python-layout-converter.git master
+git push https://github.com/poma23324/python-layout-converter.git --tags
