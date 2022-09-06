8fa0e7c5597e51a4b35f2714f08c8bfcaa5be420
Roman Smolnyk <poma23324@gmail.com>
2022-09-06 11:36:24 +0300

    [refactor] Update sh scripts

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 var=$(find . -maxdepth 1 -type f -name "*.py")
 var=$(basename "$var")
 var_ext=$(echo "$var" | cut -f 1 -d '.')
-python -m PyInstaller --icon=icon.ico --noconsole -F "$var"
+python -m PyInstaller --icon=design/icon.ico --noconsole -F "$var"
 rm -rf exe
 mkdir exe
 mv dist/"$var_ext" exe/"$var_ext"
---
+++ pull.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git pull https://gitlab.com/roman-smolnyk/python-layout-converter.git master
+git pull https://github.com/roman-smolnyk/python-layout-converter.git master
